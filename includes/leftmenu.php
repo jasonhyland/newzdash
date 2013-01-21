dca74529d694421ba5924ca3347dd5ef5b51efcf
Eric Young <eric@aceshome.com>
2013-01-21 16:39:17 -0500

    added recently added tab

--- includes/leftmenu.php
+++ includes/leftmenu.php
@@ -4,6 +4,7 @@
 		<ul class="nav nav-tabs nav-stacked main-menu">
 			<li class="nav-header hidden-tablet">Main</li>
 			<li><a class="ajax-link" href="index.php"><i class="icon-home"></i><span class="hidden-tablet"> Dashboard</span></a></li>
+			<li><a class="ajax-link" href="recent.php"><i class="icon-list-alt"></i><span class="hidden-tablet"> Recently Added</span></a></li>
 			<li><a class="ajax-link" href="statistics.php"><i class="icon-eye-open"></i><span class="hidden-tablet"> Statistics</span></a></li>
 			<li><a class="ajax-link" href="performance.php"><i class="icon-list-alt"></i><span class="hidden-tablet"> Performance</span></a></li>
 			<li><a class="ajax-link" href="system.php"><i class="icon-list-alt"></i><span class="hidden-tablet"> System</span></a></li>
